8ed54c94322077bc8aa2409037f71fcf0ba2888f
ls02 <2877455773@qq.com>
2022-04-02 21:49:26 +0800

    Directory where C code is stored

---
+++ C/test-12-22-3/test3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int g_val = 0;
+
+int main()
+{
+  pid_t id = fork();
+
+  if (id < 0)
+  {
+    perror("fork");
+
+    return 0;
+  }
+  else if (!id)
+  {
+    g_val = 100;
+    printf("chilid[%d]:%d：%p\n", getpid(), g_val, &g_val);
+  }
+  else
+  {
+    sleep(3);
+    printf("parent[%d]:%d：%p\n", getpid(), g_val, &g_val);
+  }
+
+  sleep(1);
+
+  return 0;
+}
