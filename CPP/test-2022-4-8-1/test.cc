28e05599c2c503e78a15b5b61a70b5752face7d5
ls02 <2877455773@qq.com>
2022-04-09 08:35:18 +0800

    2022

--- CPP/test-2022-4-8-1/test.cc
+++ CPP/test-2022-4-8-1/test.cc
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <string.h>
 #include <string>
 #include <fstream>
 
 int main(void)
 {
-    std::ofstream ofs("test.txt");
-    std::ifstream ifs("test.cc");
+    std::ofstream ofs("test.bin");
     
-    char ch = 0;
-    while (ifs.get(ch))
-    {
-        ofs << ch;
-    }
+    //char ch = 0;
+    //while (ifs.get(ch))
+    //{
+    //    ofs<< ch;
+    //}
+
+    const char* str = "hello";
+    ofs.write(str, strlen(str));
+    
+    std::ifstream ifs("test.bin");
+    char* s = new char[strlen(str)];
+    ifs.read(s, strlen(str)); 
+    std::cout << s << std::endl;
 
     return 0;
 }
