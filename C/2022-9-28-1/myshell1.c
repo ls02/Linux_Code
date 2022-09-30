02b72a2eeb005e9494ffdb208a2f26da4acc90a2
ls02 <2877455773@qq.com>
2022-09-30 21:33:04 +0800

    practice code

--- C/2022-9-28-1/myshell1.c
+++ C/2022-9-28-1/myshell1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -10,6 +12,7 @@
 
 int main() 
 {
+    int type = 0; // 0 >, 1 >>, 2 <
     char cmd[LEN];
     char* myarg[NUM];
 
@@ -19,6 +22,34 @@ int main()
         fgets(cmd, LEN, stdin);
         cmd[strlen(cmd) - 1] = '\0';
         myarg[0] = strtok(cmd, " ");
+
+        char* start = cmd;
+        while (*start != '\0')
+        {
+            if (*start == '>')
+            {
+                type = 0;
+                *start = '\0';
+                start++;
+
+                if (*start == '>')
+                {
+                    type = 1;
+                    start++;
+                }
+
+                break;
+            }
+            if (*start == '<')
+            {
+                type = 2;
+                *start = '\0';
+                start++;
+                break;
+            }
+
+            start++;
+        }
         int i = 1;
 
         while (myarg[i] = strtok(NULL, " "))
@@ -30,6 +61,33 @@ int main()
         if (id == 0) 
         {
             //child
+            if (NULL != start)
+            {
+                if (type == 0)
+                {
+                    int fd = open(start, O_WRONLY | O_CREAT, 0664);
+                    if (fd < 0)
+                    {
+                        perror("open");
+                        exit(2);
+                    }
+
+                    dup2(fd, 1);
+                }
+                else if (type == 1)
+                {
+
+                }
+                else if (type == 2)
+                {
+                    int fd = open(start, O_RDONLY);
+                    dup2(fd, 0);
+                }
+                else 
+                {
+
+                }
+            }
             execvp(myarg[0], myarg);
             exit(11);
             
