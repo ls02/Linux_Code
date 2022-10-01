82d334b24a714e4da99f27002fef576e88e45a05
ls02 <2877455773@qq.com>
2022-10-01 16:24:01 +0800

    practice code

--- C/2022-9-29-1/test.c
+++ C/2022-9-29-1/test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -7,11 +9,41 @@
 int main()
 {
     umask(0);
+    //close(1);
 
-    int fd = open("log.txt", O_WRONLY | O_CREAT, 0666);
+    //int fd = open("log.txt", O_WRONLY | O_CREAT, 0666);
+    //int fd = open("log.txt", O_WRONLY | O_APPEND, 0666);
+    int fd = open("log.txt", O_RDONLY, 0666);
+    if (fd == -1)
+    {
+        perror("open failed!\n");
+        exit(1);
+    }
+    else 
+    {
+        printf("open success!\n");
+    }
 
-    printf("fd: %d\n", fd);
+    //printf("fd: %d\n", fd);
+   // char* ch = "hello\n";
+   // int count = 0;
+   // //dup2(fd, 1);
+   // 
+   // 
+   // while (count < 3)
+   // {
+   //     //write(fd, ch, strlen(ch));
+   //     write(1, ch, strlen(ch));
+   //     count++;
+   // }
+   
+    char ch[3] = { 0 };
+    while (read(fd, ch, sizeof(ch)))
+    {
+        printf("%s", ch);
+    }
 
+    close(fd);
 
     return 0;
 }
